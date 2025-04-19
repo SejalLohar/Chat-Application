3c6be84cace0d6a6ea1175e24653fd4c174ab274
karmanya2448 <karmanya.kumra@gmail.com>
2025-04-19 16:54:50 +0530

    Delete forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import RegisteredUser
-
-class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        model = RegisteredUser
-        fields = ['username', 'email', 'password']
